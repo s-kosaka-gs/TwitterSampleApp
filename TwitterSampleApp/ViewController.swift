aeac6839c7f3c8c3ab539001e71d8f87695b8c39
Sumire Kosaka <s.kosaka@genbasupport.com>
2024-11-27 11:46:55 +0900

    Initial Commit

---
+++ TwitterSampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwitterSampleApp
+//
+//  Created by 髙坂澄怜 on 2024/11/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TwitterSampleAppTests/TwitterSampleAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSampleAppTests.swift
+//  TwitterSampleAppTests
+//
+//  Created by 髙坂澄怜 on 2024/11/27.
+//
+
+import Testing
+@testable import TwitterSampleApp
+
+struct TwitterSampleAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
